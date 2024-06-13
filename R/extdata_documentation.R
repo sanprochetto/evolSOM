edc1bb0a11e0adb34044ee17326d8cc7ea052212
sanprochetto <sprochetto@gmail.com>
2024-06-13 16:32:03 -0300

    added raw example data

---
+++ R/extdata_documentation.R
@@ -0,0 +1,35 @@
+#' Data Set C3
+#'
+#' This is a sample dataframe included in the package for demonstration.
+#'
+#' @format A data frame with `9757` rows and `4` columns:
+#' @source \url{https://github.com/sanprochetto/evolSOM/inst/extdata}
+#'
+#'@name dataC3.csv
+
+#' Data Set C4
+#'
+#' This is a sample dataframe included in the package for demonstration.
+#'
+#' @format A data frame with `9757` rows and `4` columns:
+#' @source \url{https://github.com/sanprochetto/evolSOM/inst/extdata}
+#'
+#'@name dataC4.csv
+
+#' Data Set PK
+#'
+#' This is a sample dataframe included in the package for demonstration.
+#'
+#' @format A data frame with `9757` rows and `4` columns:
+#' @source \url{https://github.com/sanprochetto/evolSOM/inst/extdata}
+#'
+#'@name dataPK.csv
+
+#' Data Set Class_SOM
+#'
+#' This is a sample dataframe included in the package for demonstration.
+#'
+#' @format A data frame with `9757` rows and `4` columns:
+#' @source \url{https://github.com/sanprochetto/evolSOM/inst/extdata}
+#'
+#'@name class_SOM.csv
